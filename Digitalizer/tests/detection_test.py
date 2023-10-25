17ef605a5405599bb3eef1eb9407c76506bec360
duducaa <duduca1881@gmail.com>
2023-10-24 23:17:47 -0300

    created the class Digitalizer with batch read and filter by regular expressions

--- Digitalizer/tests/detection_test.py
+++ Digitalizer/tests/detection_test.py
@@ -2,38 +2,28 @@ from paddleocr import PaddleOCR
 import os
 import cv2 as cv
 import re
-
-class Digitalizer(PaddleOCR):
-    def __init__(self, main_path, **kwargs):
-        super().__init__(**kwargs)
-        self.images_path = [os.path.join(main_path, file) for file in os.listdir(main_path)]
-        self.results = []
-        
-    def detect_text(self):
-        for path in self.images_path:
-            image = cv.imread(path)
-            result = self.ocr(image, cls=True)
-            self.results.append(result)
+from digitalizer import Digitalizer
 
 main_path = 'Digitalizer/tests/detection_data'
-ocr = Digitalizer(main_path, use_angle_cls=True, lang='pt')
-for index, path in enumerate(ocr.images_path):
-    image = cv.imread(path)
-    
-    results = ocr.ocr(image, cls=True)
-    for line in results[0]:
-        word = line[1][0]
-        rect = [list(map(int, i)) for i in line[0]]
+images_paths = [os.path.join(main_path, file) for file in os.listdir(main_path)]
+digitalizer = Digitalizer(images_paths, use_angle_cls=True, lang='pt')
+digitalizer.detect_text('Phone')
+
+# for image, result in zip(digitalizer.images, digitalizer.results):
+#     for line in result[0]:
+        # word = line[1][0]
+        # rect = [list(map(int, i)) for i in line[0]]
         
-        area_1 = abs((rect[1][0] - rect[3][0]) * (rect[1][1] - rect[3][1]))
-        area_2 = abs((rect[0][0] - rect[2][0]) * (rect[0][1] - rect[2][1]))
+        # area_1 = abs((rect[1][0] - rect[3][0]) * (rect[1][1] - rect[3][1]))
+        # area_2 = abs((rect[0][0] - rect[2][0]) * (rect[0][1] - rect[2][1]))
         
-        if area_1 > area_2:
-            cv.rectangle(image, rect[1], rect[3], (255, 0, 0), 3)
-        else:
-            cv.rectangle(image, rect[0], rect[2], (255, 0, 0), 3)
+        # if area_1 > area_2:
+        #     cv.rectangle(image, rect[1], rect[3], (255, 0, 0), 3)
+        # else:
+        #     cv.rectangle(image, rect[0], rect[2], (255, 0, 0), 3)
     
-    cv.imshow('Display', image)
-    cv.waitKey()
-    cv.destroyAllWindows()
+    # cv.imshow('Display', image)
+    # cv.waitKey()
+    # cv.destroyAllWindows()
     
+print(digitalizer.results['Phone'])    
---
+++ Digitalizer/tests/digitalizer.py
@@ -0,0 +1,29 @@
+from paddleocr import PaddleOCR
+import re
+
+class Digitalizer(PaddleOCR):
+    def __init__(self, paths, **kwargs):
+        super().__init__(**kwargs)
+        self.images = [cv.imread(path) for path in paths]
+        self.patterns = {'Instagram': '', 'Facebook': '', 'Email': '', 'Phone': ''}
+        self.results = {'Instagram': [], 'Facebook': [], 'Email': [], 'Phone': []}
+    
+    def match_pattern(self, word, specific_pattern=''):
+        if specific_pattern != '':
+            sp_pattern = self.patterns[specific_pattern]
+            return re.match(sp_pattern, word), specific_pattern
+        else:
+            for font, pattern in self.patterns.items():
+                if re.match(pattern, word):
+                    return True, font
+        
+        return False, ''
+    
+    def detect_text(self, specific_pattern=''):
+        for image in self.images:
+            result = self.ocr(image, cls=True)
+            for line in result[0]:
+                word = line[1][0]
+                matches, font = self.match_pattern(word, specific_pattern)
+                if not matches: continue
+                self.results[font].append(word)
