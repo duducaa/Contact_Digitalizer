20684efbd95e691337e881f2730b8e18a5dcdf7d
duducaa <duduca1881@gmail.com>
2023-11-27 06:30:46 -0300

    start the verify step

--- Digitalizer/data/models/digitalizer.py
+++ Digitalizer/data/models/digitalizer.py
@@ -1,16 +1,23 @@
 from paddleocr import PaddleOCR
 import re
 import cv2 as cv
+import requests
 
 class Digitalizer(PaddleOCR):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.patterns = {
-            'Instagram': r'^@', 
-            #'Facebook': '',
-            'Site': r'\b(www\.)?w+\.\w{3}(\.\w{2})?\b',
-            'Email': r'\b\w+@\w+(\.\w{2, 3}){1, 2}', 
+            'Email': r'\w+@\w+\.\w{3}\.\w{2}', 
+            'Instagram': r'@[\w\.]+',
+            'Site': r'www\.\w+\.\w{3}\.\w{2}(/\w+)?|\w+\.\w{3}\.\w{2}(/\w+)?',
             'Phone': r'\(?\d{2,3}\)?[-.\s]?9?[-.\s]?\d{4}[-.\s]?\d{4}|\b0800?\s?\d{3}\s?\d{4}\b|\b9?[-.\s]?\d{4}[-.\s]?\d{4}\b'}
+        
+        self.verify_functions = {
+            'Instagram': self.verify_instagram,
+            'Site': self.verify_site,
+            'Email': self.verify_email,
+            'Phone': self.verify_phone
+        }
 
     def match_patterns(self, word, score, contacts):
         for font, pattern in self.patterns.items():
@@ -18,14 +25,28 @@ class Digitalizer(PaddleOCR):
             matches = re.findall(pattern, word)
             for match in matches:
                 contacts[font].append([match, score])
-            if len(matches) > 0: break
     
     def detect_text(self, image):
         result = self.ocr(image, cls=True)
         contacts = {key: [] for key in self.patterns.keys()}
         for line in result[0]:
             word = line[1][0]
+            print(word)
             score = line[1][1]
             self.match_patterns(word, score, contacts)
 
         return contacts
+    
+    def verify_instagram(self, account):
+        pass
+    
+    def verify_site(self, site):
+        response = requests.get(f"https://{site}")
+        print(response.status_code)
+        return response.status_code == 200
+    
+    def verify_email(self, email):
+        pass
+    
+    def verify_phone(self, phone):
+        pass
--- Digitalizer/tests/detection_test/detection_test.py
+++ Digitalizer/tests/detection_test/detection_test.py
@@ -1,30 +1,3 @@
-import cv2 as cv
-import requests
-import json
-import base64
-import os
+import re
 
-main_path = 'tests/detection_test/detection_data'
-images_paths = [os.path.join(main_path, file) for file in os.listdir(main_path)]
-
-BASE_URL = 'http://127.0.0.1:5000/'
-headers = {'Content-type': 'application/json; charset=utf-8'}
-
-for path in images_paths:
-    image = cv.imread(path)
-    json_image = json.dumps(image.tolist())
-    encoded = base64.b64encode(json_image.encode())
-    data = json.dumps([{'image': encoded.decode('utf8')}])
-
-    response = requests.post(BASE_URL + 'detection', data=data, headers=headers)
-
-    contacts = response.json()['contacts']
-    for key, value in contacts.items():
-        print('========================\n')
-        print(f'        {key}           \n')
-        for v in value:
-            print(v)
-        print('\n========================')
-
-    cv.imshow('Display', image)
-    cv.waitKey()
+print(re.findall(r'www\.\w+\.\w{3}\.\w{2}(/\w+)?|\w+\.\w{3}\.\w{2}(/\w+)?', 'multivix.edu.br/ead'))
